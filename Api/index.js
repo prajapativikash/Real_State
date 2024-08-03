0f93250d7a2a4f1bf0799cdf6606dcee73f4ebc8
vikash prajapati <vikashprajapati6742@gmail.com>
2024-08-04 00:10:57 +0530

    connect to database

--- Api/index.js
+++ Api/index.js
@@ -1,5 +1,14 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+   console.log("connected to Db succesfull");
+})
 
+   .catch((error) => {
+      console.log(error)
+   })
 const app = express();
 
 app.listen(4000, () => {
